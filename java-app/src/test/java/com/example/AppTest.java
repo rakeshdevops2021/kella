274c7a76d3142a898d66d51d5ea567ba382d6ebd
rakeshdevops2021 <87441171+rakeshdevops2021@users.noreply.github.com>
2025-09-04 12:57:33 +0530

    Create AppTest.java

---
+++ java-app/src/test/java/com/example/AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void testAdd() {
+        assertEquals(3, com.example.App.add(1, 2));
+    }
+}
